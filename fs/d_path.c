alaahl/linux

commit 7e5f7bb08b8cefd3a7e8961861f47fe1f0e830d4

unexport simple_dname()
Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

fs/d_path.c
@@ -316,7 +316,6 @@ char *simple_dname(struct dentry *dentry, char *buffer, int buflen)
 		end = ERR_PTR(-ENAMETOOLONG);
 	return end;
 }
-EXPORT_SYMBOL(simple_dname);
 
 /*
  * Write full pathname from the root of the filesystem into the buffer.

fs/internal.h
@@ -160,6 +160,7 @@ extern int d_set_mounted(struct dentry *dentry);
 extern long prune_dcache_sb(struct super_block *sb, struct shrink_control *sc);
 extern struct dentry *d_alloc_cursor(struct dentry *);
 extern struct dentry * d_alloc_pseudo(struct super_block *, const struct qstr *);
+extern char *simple_dname(struct dentry *, char *, int);
 
 /*
  * read_write.c

include/linux/dcache.h
@@ -291,7 +291,6 @@ static inline unsigned d_count(const struct dentry *dentry)
  */
 extern __printf(4, 5)
 char *dynamic_dname(struct dentry *, char *, int, const char *, ...);
-extern char *simple_dname(struct dentry *, char *, int);
 
 extern char *__d_path(const struct path *, const struct path *, char *, int);
 extern char *d_absolute_path(const struct path *, char *, int);

commit f7b6222bbc1c748613124de96111d958c34a9d68

fs: Add function declaration of simple_dname
The simple_dname() is declared in internal header file but the
declaration is missing in d_path.c.

The compilation with W=1 generates the following GCC warning.

fs/d_path.c:311:7: warning: no previous prototype for 'simple_dname' [-Wmissing-prototypes]
  311 | char *simple_dname(struct dentry *dentry, char *buffer, int buflen)
      |       ^~~~~~~~~~~~

Fixes: 7e5f7bb08b8c ("unexport simple_dname()")
Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

fs/d_path.c
@@ -8,6 +8,8 @@
 #include <linux/prefetch.h>
 #include "mount.h"
 
+#include "internal.h"
+
 static int prepend(char **buffer, int *buflen, const char *str, int namelen)
 {
 	*buflen -= namelen;